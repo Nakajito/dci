55035845f7a5e8aedd98e8ba6c7841c899e95327
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-08-06 21:17:51 +0000

    se instalaron extensiones

--- ausencias/models.py
+++ ausencias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Militar(models.Model):
+    grado = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=150)
+    matricula = models.CharField(max_length=9)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Permiso(models.Model):
+    fecha = models.DateField()
+    motivo = models.TextField()
+    hora_llegada = models.TimeField()
+    hora_salida = models.TimeField()
+    militar = models.ForeignKey(Militar, on_delete = models.CASCADE)
