c9d135304d96502dac3df4e90e6f35633a2a42c4
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-08-06 21:17:39 +0000

    se instalaron extensiones

--- dci/settings.py
+++ dci/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap5",
+    "ausencias",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
